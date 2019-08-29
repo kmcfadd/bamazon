b798c6493d2f8f118bb5bd2f25e2930c397a3d0a
Kyle McFadden <burntwist@sbcglobal.net>
2019-08-29 10:51:03 -0700

    fix connection error with port, display products table upon app loading with selection question

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var inquirer = require("inquirer")
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3006,
+    port: 3306,
     user: "root",
     password: "Ky27Ls$10pQ9d",
     database: "bamazon_DB"
@@ -11,36 +11,43 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
+    start()
 })
 
-connection.query("SELECT id, product_name, price FROM products", function (err, res) {
-    if (err) throw err;
+function start() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log("--------- Products for Sale ----------")
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + " " + res[i].product_name + " $" + res[i].price)
+        }
 
-    inquirer
-        .prompt([{
-                name: "pick",
-                type: "rawlist",
-                choices: function () {
-                    var items = [];
-                    for (var i = 0; i < res.length; i++) {
-                        items.push(res[i].id);
-                    }
-                    return items;
+        inquirer
+            .prompt([{
+                    name: "pick",
+                    type: "rawlist",
+                    choices: function () {
+                        var items = [];
+                        for (var i = 0; i < res.length; i++) {
+                            items.push(res[i].id);
+                        }
+                        return items;
+                    },
+                    message: "Which item would you like to buy?"
                 },
-                message: "Which item would you like to buy?"
-            },
-            {
-                name: "count",
-                type: "input",
-                message: "How many would you like to purchase?"
-            }
-        ])
-        .then(function (ans) {
-            var selection;
-            for (var i = 0; i < res.length; i++) {
-                if (res[i].id === ans.pick) {
-                    selection = res[i];
+                {
+                    name: "count",
+                    type: "input",
+                    message: "How many would you like to purchase?"
                 }
-            }
-        })
-})
+            ])
+            .then(function (ans) {
+                var selection;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].id === ans.pick) {
+                        selection = res[i];
+                    }
+                }
+            })
+    })
+}
