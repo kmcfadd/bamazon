1f561de6541254700bd8f22e4602f3b79455e0bb
Kyle McFadden <burntwist@sbcglobal.net>
2019-08-29 10:38:34 -0700

    insert products into database table and start functionality for user interaction

--- app.js
+++ app.js
@@ -12,3 +12,35 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
 })
+
+connection.query("SELECT id, product_name, price FROM products", function (err, res) {
+    if (err) throw err;
+
+    inquirer
+        .prompt([{
+                name: "pick",
+                type: "rawlist",
+                choices: function () {
+                    var items = [];
+                    for (var i = 0; i < res.length; i++) {
+                        items.push(res[i].id);
+                    }
+                    return items;
+                },
+                message: "Which item would you like to buy?"
+            },
+            {
+                name: "count",
+                type: "input",
+                message: "How many would you like to purchase?"
+            }
+        ])
+        .then(function (ans) {
+            var selection;
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].id === ans.pick) {
+                    selection = res[i];
+                }
+            }
+        })
+})
--- data.sql
+++ data.sql
@@ -16,3 +16,56 @@ CREATE TABLE products
   PRIMARY KEY
     (id)
 );
+
+    insert into products
+        (product_name, department_name, price, stock_quantity)
+    values
+        ("iPhone", "Technology", 600, 5000);
+
+    insert into products
+        (product_name, department_name, price, stock_quantity)
+    values
+        ("PS4", "Technology", 250, 2000);
+
+    insert into products
+        (product_name, department_name, price, stock_quantity)
+    values
+        ("Winter Coat", "Apparel", 40, 200);
+
+    insert into products
+        (product_name, department_name, price, stock_quantity)
+    values
+        ("Candy", "Food", 1, 1000);
+
+    insert into products
+        (product_name, department_name, price, stock_quantity)
+    values
+        ("Socks", "Apparel", 15, 250);
+
+    insert into products
+        (product_name, department_name, price, stock_quantity)
+    values
+        ("Fortnite", "Technology", 30, 99999);
+
+    insert into products
+        (product_name, department_name, price, stock_quantity)
+    values
+        ("Lego Set", "Toys", 40, 750);
+
+    insert into products
+        (product_name, department_name, price, stock_quantity)
+    values
+        ("Barbie Doll", "Toys", 25, 500);
+
+    insert into products
+        (product_name, department_name, price, stock_quantity)
+    values
+        ("Coffee Blends", "Food", 30, 1200);
+
+    insert into products
+        (product_name, department_name, price, stock_quantity)
+    values
+        ("Backpack", "Apparel", 50, 900);
+
+    select *
+    from products;
